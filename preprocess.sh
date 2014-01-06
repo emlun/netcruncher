d1c46c3eecc93a9b5f1235f2de9467e7a33740a5
Emil Lundberg <lundberg.emil@gmail.com>
2014-01-07 01:03:04 +0900

    Wrap faction replacement code in switchable if statement

--- preprocess.sh
+++ preprocess.sh
@@ -11,17 +11,21 @@ if [[ ! -d "$OUTPUT_DIRECTORY" ]]; then
     fi
 fi
 
+replace_factions=true
+
 cp "$1" "$FILE"
 
 # Replace faction name with numeral ID
 
-name_id=0
-OLDIFS="$IFS"
-IFS=$'\n'
-for name in $( (cut -d , -f 3 "$FILE"; cut -d , -f 5 "$FILE") | sort -u | grep -v Faction); do
+if $replace_factions; then
+    name_id=0
+    OLDIFS="$IFS"
+    IFS=$'\n'
+    for name in $( (cut -d , -f 3 "$FILE"; cut -d , -f 5 "$FILE") | sort -u | grep -v Faction); do
     echo "Replacing ${name} with ${name_id} ..."
     echo $name_id $name >> "$LEGEND_FACTIONS"
     sed -i "s#${name}#${name_id}#g" "$FILE"
     name_id=$(($name_id+1))
-done
-IFS="$OLDIFS"
+    done
+    IFS="$OLDIFS"
+fi
