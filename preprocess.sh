9b5f239f3ad517bba99ab069ae356c568c10a83a
Emil Lundberg <lundberg.emil@gmail.com>
2014-01-07 00:20:37 +0900

    Replace faction names with numeral IDs

---
+++ preprocess.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+OUTPUT_DIRECTORY=output
+FILE="$OUTPUT_DIRECTORY/processed.csv"
+LEGEND_FACTIONS="$OUTPUT_DIRECTORY/legend-factions.txt"
+
+if [[ ! -d "$OUTPUT_DIRECTORY" ]]; then
+    if ! mkdir "$OUTPUT_DIRECTORY"; then
+        echo "Could not create output directory $OUTPUT_DIRECTORY, aborting..."
+        exit 1
+    fi
+fi
+
+cp "$1" "$FILE"
+
+# Replace faction name with numeral ID
+
+name_id=0
+OLDIFS="$IFS"
+IFS=$'\n'
+for name in $( (cut -d , -f 3 "$FILE"; cut -d , -f 5 "$FILE") | sort -u | grep -v Faction); do
+    echo "Replacing ${name} with ${name_id} ..."
+    echo $name_id $name >> "$LEGEND_FACTIONS"
+    sed -i "s#${name}#${name_id}#g" "$FILE"
+    name_id=$(($name_id+1))
+done
+IFS="$OLDIFS"
