5be0fd867e09f53e90f652b3697a1581ddd02eda
Emil Lundberg <lundberg.emil@gmail.com>
2014-01-07 23:13:01 +0900

    Rename stats matrix to matches

--- read_stats.m
+++ read_stats.m
@@ -1,5 +1,5 @@
 % Corp_Player,Player_Faction,Runner_Player,Opponent_Faction,Result,Turns_Played,Win,Influence,OpInf,Corp_score,Runner_score,Agendas.Nr.,Corp.Deck.Size,Runner.Deck.Size
-stats=csvread('output/processed.csv', 1, 0);
+matches=csvread('output/processed.csv', 1, 0);
 
 Corp_Player = 1;
 Player_Faction = 2;
