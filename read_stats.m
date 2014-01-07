e4daa7f791121781cf5c3036df40491098795ba4
Emil Lundberg <lundberg.emil@gmail.com>
2014-01-08 00:45:04 +0900

    Add output/ to MATLAB path

--- read_stats.m
+++ read_stats.m
@@ -1,5 +1,7 @@
 % Corp_Player,Player_Faction,Runner_Player,Opponent_Faction,Result,Turns_Played,Win,Influence,OpInf,Corp_score,Runner_score,Agendas.Nr.,Corp.Deck.Size,Runner.Deck.Size
-matches=csvread('output/processed.csv', 1, 0);
+path(path,'output')
+
+matches=csvread('processed.csv', 1, 0);
 
 Corp_Player = 1;
 Player_Faction = 2;
