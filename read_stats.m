0888ce4bf15b552d62d052c71a16a5944dfd1a2f
Emil Lundberg <lundberg.emil@gmail.com>
2014-01-07 19:09:05 +0900

    Add column legends to read_stats.m

--- read_stats.m
+++ read_stats.m
@@ -1,1 +1,17 @@
+% Corp_Player,Player_Faction,Runner_Player,Opponent_Faction,Result,Turns_Played,Win,Influence,OpInf,Corp_score,Runner_score,Agendas.Nr.,Corp.Deck.Size,Runner.Deck.Size
 stats=csvread('output/processed.csv', 1, 0);
+
+Corp_Player = 1;
+Player_Faction = 2;
+Runner_Player = 3;
+Opponent_Faction = 4;
+Result = 5;
+Turns_Played = 6;
+Win = 7;
+Influence = 8;
+OpInf = 9;
+Corp_score = 10;
+Runner_score = 11;
+Agendas_Nr = 12;
+Corp_Deck_Size = 13;
+Runner_Deck_Size = 14;
