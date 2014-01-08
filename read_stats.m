81eca9feec6454fd29a51bcdbd625f60b002f542
Emil Lundberg <lundberg.emil@gmail.com>
2014-01-09 02:31:56 +0900

    Update read_stats.m to reflect preprocessor changes

--- read_stats.m
+++ read_stats.m
@@ -3,7 +3,9 @@ matches=csvread('preprocessed/data.csv', 1, 0);
 addpath('preprocessed')
 
 column_enumeration;
-legend_factions;
 legend_results;
+
+cardgamedb_colors;
+
 factions_corp;
 factions_runner;
