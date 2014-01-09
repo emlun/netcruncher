1e769865fdd6b1d11324801d177271dc75c56845
Emil Lundberg <lundberg.emil@gmail.com>
2014-01-09 17:47:26 +0900

    Add copyright notices to all source files
    Except align_pie_labels.m since that code is copypasted from MathWorks.

--- run_octave.m
+++ run_octave.m
@@ -1,2 +1,18 @@
+% Copyright 2014 Emil Lundberg
+%
+% This file is part of Netcruncher.
+%
+% Netcruncher is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 2 of the License, or
+% (at your option) any later version.
+%
+% Netcruncher is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Netcruncher.  If not, see <http://www.gnu.org/licenses/>.
 pkg load statistics
 crunch
